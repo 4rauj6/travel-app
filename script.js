de029133de8bdfd095656304992cb91faf004edf
João Victor <159325010+4rauj6@users.noreply.github.com>
2026-04-09 09:47:15 -0300

    Add script.js to the travel-app repository

---
+++ script.js
@@ -0,0 +1,12 @@
+function showSidebar(){
+    const showSiberToggle = document.getElementById("sidebar");
+    const iconForBtn = document.getElementById("iconBtn");
+    if(showSiberToggle.style.display === "none"){
+        showSiberToggle.style.display = "block";
+        iconForBtn.classList.replace("fa-bars", "fa-close");
+
+    }else{
+        showSiberToggle.style.display = "none";
+        iconForBtn.classList.replace("fa-close", "fa-bars");
+    }
+}
