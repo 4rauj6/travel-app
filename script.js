f69394f956cdf0757b9da4b44e83bbedb4142b46
João Victor <159325010+4rauj6@users.noreply.github.com>
2026-04-10 16:03:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-function showSidebar(){
-    const showSiberToggle = document.getElementById("sidebar");
-    const iconForBtn = document.getElementById("iconBtn");
-    if(showSiberToggle.style.display === "none"){
-        showSiberToggle.style.display = "block";
-        iconForBtn.classList.replace("fa-bars", "fa-close");
-
-    }else{
-        showSiberToggle.style.display = "none";
-        iconForBtn.classList.replace("fa-close", "fa-bars");
-    }
-}
-
 function showMore() {
     const gotToHomeDiv = document.getElementById('locais-mais-escolhidos');
     gotToHomeDiv.scrollIntoView({ behavior: 'smooth' });
