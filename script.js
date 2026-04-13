33dd11b0c4ecc7cbf604b4713ae1e91e345789f5
João Victor <159325010+4rauj6@users.noreply.github.com>
2026-04-13 16:27:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ function showMore() {
 }
 
 function openSidebar() {
-  const openSidebarEvent = document.getElementById('sidebar-list');
-  const sidebarMenuButton = document.querySelector('.fa-bars, .fa-times');
-  
-  if(openSidebarEvent.style.display === 'block'){
-    openSidebarEvent.style.display = 'none';
-  }else{
-    openSidebarEvent.style.display = 'block';
-  }
-  sidebarMenuButton.classList.toggle('fa-bars');
-  sidebarMenuButton.classList.toggle('fa-times');
+    const openSidebarEvent = document.getElementById('sidebar-list');
+    const sidebarMenuButton = document.querySelector('.fa-bars, .fa-times');
+
+    if (openSidebarEvent.style.display === 'block') {
+        openSidebarEvent.style.display = 'none';
+    } else {
+        openSidebarEvent.style.display = 'block';
+    }
+    sidebarMenuButton.classList.toggle('fa-bars');
+    sidebarMenuButton.classList.toggle('fa-times');
 }
 
 function sortearSelecionados() {
