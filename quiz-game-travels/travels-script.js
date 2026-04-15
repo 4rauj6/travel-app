5ddea010df54c33ec0dc888573657b60ae2d49f3
João Victor <159325010+4rauj6@users.noreply.github.com>
2026-04-15 14:30:04 -0300

    Update travels-script.js

---
+++ quiz-game-travels/travels-script.js
@@ -0,0 +1,9 @@
+function startGame() {
+    document.querySelector('.homepage-game').style.display = 'none';
+    document.querySelector('.game-page').style.display = 'block';
+}
+
+function returnToHome() {
+    document.querySelector('.game-page').style.display = 'none';
+    document.querySelector('.homepage-game').style.display = 'block';
+}
