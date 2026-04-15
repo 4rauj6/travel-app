80c7ae2d91abf8b7fdbd63c31a77217ccc09b351
João Victor <159325010+4rauj6@users.noreply.github.com>
2026-04-15 16:02:05 -0300

    Delete quiz-game-travels directory

--- quiz-game-travels/travels-script.js
+++
@@ -1,9 +0,0 @@
-function startGame() {
-    document.querySelector('.homepage-game').style.display = 'none';
-    document.querySelector('.game-page').style.display = 'block';
-}
-
-function returnToHome() {
-    document.querySelector('.game-page').style.display = 'none';
-    document.querySelector('.homepage-game').style.display = 'block';
-}
