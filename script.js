f4ba581f6dfde4dbadd022709606254a812292f2
João Victor <159325010+4rauj6@users.noreply.github.com>
2026-04-27 09:47:29 -0300

    New files has been added
    New files for the places page has been added to the main branch

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function showMore() {
     const gotToHomeDiv = document.getElementById('locais-mais-escolhidos');
     gotToHomeDiv.scrollIntoView({ behavior: 'smooth' });
-}
+};
 
 function openSidebar() {
     const openSidebarEvent = document.getElementById('sidebar-list');
@@ -14,30 +14,4 @@ function openSidebar() {
     }
     sidebarMenuButton.classList.toggle('fa-bars');
     sidebarMenuButton.classList.toggle('fa-times');
-}
-
-/*function sortearSelecionados() {
-    const checkboxes = document.querySelectorAll('.checkboxes input:checked');
-    const selecionados = [];
-
-    checkboxes.forEach(cb => {
-        selecionados.push(cb.value);
-    });
-
-    if (selecionados.length === 0) {
-        alert("Selecione pelo menos um país!");
-        return;
-    }
-
-    const sorteado = selecionados[Math.floor(Math.random() * selecionados.length)];
-
-    document.getElementById("resultado-sorteio").innerText = "🌍 Destino: " + sorteado;
-}
-
-function sortearTodos() {
-    const todos = ["França", "Brasil", "Japão", "Itália", "Alemanha", "Espanha"];
-
-    const sorteado = todos[Math.floor(Math.random() * todos.length)];
-
-    document.getElementById("resultado-sorteio").innerText = "🌍 Destino: " + sorteado;
-}*/
+};
