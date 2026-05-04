4cb59d2ecc1ebb9aa9b883ca040d83f8dec824c9
João Victor <159325010+4rauj6@users.noreply.github.com>
2026-05-04 08:47:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,52 @@ function openSidebar() {
     sidebarMenuButton.classList.toggle('fa-bars');
     sidebarMenuButton.classList.toggle('fa-times');
 };
+
+function showBarcelonaPage() {
+    const getTrigger = document.querySelectorAll('.homepage, .locais-page, .locais-descobrir, .tokyo-page, .footer-content');
+    const getPage = document.querySelector('.barcelona-page');
+
+    if (getPage.style.display === 'flex') {
+        getPage.style.display = 'none';
+        getTrigger.forEach(section => {
+            section.style.display = 'block';
+        });
+    } else {
+        getPage.style.display = 'block';
+        getTrigger.forEach(section => {
+            section.style.display = 'none';
+        });
+    }
+}
+
+function showTokyoPage() {
+    const getTrigger = document.querySelectorAll('.homepage, .locais-page, .locais-descobrir, .barcelona-page');
+    const getPage = document.querySelector('.tokyo-page');
+
+    if (getPage.style.display === 'flex') {
+        getPage.style.display = 'none';
+        getTrigger.forEach(section => {
+            section.style.display = 'block';
+        });
+    } else {
+        getPage.style.display = 'flex';
+        getTrigger.forEach(section => {
+            section.style.display = 'none';
+        });
+    }
+
+}
+
+
+function closePages() {
+    const getTrigger = document.querySelectorAll('.homepage, .locais-page, .locais-descobrir');
+    const getPage = document.querySelector('.barcelona-page, .tokyo-page');
+
+    if (getPage.style.display = 'none') {
+        getTrigger.forEach(section => {
+            section.style.display = '';
+        });
+    } else {
+        getPage.style.display = 'flex';
+    };
+}
